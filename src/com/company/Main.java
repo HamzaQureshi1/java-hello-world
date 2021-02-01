928aa8b9695504860b6f3b746bf1b7c3edd2f125
hqureshi6594@gmail.com <Hamzason1!>
2021-02-01 19:44:01 +0000

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.awt.*;
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+       int [][] numbers = new int[2][2];
+numbers[0][0] = 1;
+numbers[0][1] = 2;
+numbers[1][0] =3;
+numbers[1][1] =4;
+
+
+    }
+}
